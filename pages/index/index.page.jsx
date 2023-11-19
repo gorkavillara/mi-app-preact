01331ddcdddd137136b00d2550a988c61e98fa31
gorkavillar <gorka@gorkavillar.com>
2023-11-19 23:13:44 +0100

    Signals en Preact

--- pages/index/index.page.jsx
+++ pages/index/index.page.jsx
@@ -1,5 +1,13 @@
+import { signal } from "@preact/signals";
 import "./style.css";
 
+const contador = signal(0);
+
 export function Page() {
-  return <h1>Hola mundo desde Preact!</h1>;
+  return (
+    <div>
+      <h1>Hola mundo desde Preact!</h1>
+      <button onClick={() => contador.value++}>{contador}</button>
+    </div>
+  );
 }
